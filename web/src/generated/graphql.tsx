c8598fe67e93ed2307a3aff0025931cacf157f29
Stefan Andrei <stefandrei123@gmail.com>
2021-02-10 14:43:13 +0100

    implemented logout functionality in frontend

--- web/src/generated/graphql.tsx
+++ web/src/generated/graphql.tsx
@@ -232,6 +232,14 @@ export type LoginMutation = (
   ) }
 );
 
+export type LogoutMutationVariables = Exact<{ [key: string]: never; }>;
+
+
+export type LogoutMutation = (
+  { __typename?: 'Mutation' }
+  & Pick<Mutation, 'logout'>
+);
+
 export type CreateOrderMutationVariables = Exact<{
   reservationId: Scalars['Float'];
 }>;
@@ -343,6 +351,15 @@ export const LoginDocument = gql`
 
 export function useLoginMutation() {
   return Urql.useMutation<LoginMutation, LoginMutationVariables>(LoginDocument);
+};
+export const LogoutDocument = gql`
+    mutation Logout {
+  logout
+}
+    `;
+
+export function useLogoutMutation() {
+  return Urql.useMutation<LogoutMutation, LogoutMutationVariables>(LogoutDocument);
 };
 export const CreateOrderDocument = gql`
     mutation CreateOrder($reservationId: Float!) {
